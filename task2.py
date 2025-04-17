acf3fe757b1749aca3cf8c4129312b34ee3e5f2d
xx295 <raokunal0813@gmail.com>
2025-04-17 12:34:03 +1200

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,11 @@
+person = {
+    'name': 'Alice',
+    'age': 25,               #person details
+    'city': 'Auckland'
+}
+print("dictionery:", person)
+person ['email'] = "alice@email.com" #add emails
+print("after adding email:",person)
+person ['age'] = 26 #add update to her age
+print("update age:",person)
+print(person) #last print person

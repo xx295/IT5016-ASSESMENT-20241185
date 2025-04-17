7effb5f356e0b17294d2712bfb4e6a6bb386eefe
xx295 <raokunal0813@gmail.com>
2025-04-17 13:01:10 +1200

    Add files via upload

---
+++ kunal.py
@@ -0,0 +1,30 @@
+from multiprocessing.connection import answer_challenge
+
+print("hello")
+n1="hello"
+n2="kunal"
+print(n1+" "+n2)
+a=2
+b=4.2468
+ans=a+b
+print(ans)
+name=input("enter your name")
+print("hello"+"name")
+num1 = int(input("enter first num: "))
+num2 = int(input("enter the second num:"))
+solution= num1 + num2
+print(solution)
+
+names=input("enter your name")
+
+age= int(input("enter age"))
+print(age)
+#calculate year
+year= int(input("current year"))
+solution=year-age
+print(solution)
+print(name+"you arre"+str(age)+"you are born in"+str(solution))
+
+
+
+

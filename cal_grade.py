a8333d0996ce957a8e055fc55937d20c65f32276
xx295 <raokunal0813@gmail.com>
2025-04-17 12:33:06 +1200

    Add files via upload

---
+++ cal_grade.py
@@ -0,0 +1,10 @@
+numbers= list(range(1,11)) # create lists of number 1 to 10
+
+numbers.append(11)
+numbers.append(12)# added these 3 number in the lists
+numbers.append(13)
+numbers.remove(5) #removed number 5 from the lists
+print(numbers) # print all the numbers
+# THIS WAS TASK 1
+
+
